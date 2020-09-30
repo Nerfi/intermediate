4f9db24c600cc84701a6acbf40514c67e98c58f5
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-09-30 13:16:53 +0200

    install router and boostrap libraries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavbarComponent from './Components/UI/Navbar';
+import NavbarComponent from '../Components/UI/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './Components/LandingPage';
 import { BrowserRouter as Router, Route,  Switch } from "react-router-dom";
@@ -11,7 +11,7 @@ import SignIn from '../Auth/SignIn';
 import SinglePost from './Components/SinglePost';
 import EditPost from './Components/EditPost';
 //importing the context in order to listen when the user auth state changes
-import {AuthProvider} from './src/Components/UserContext/AuthContext';
+import {AuthProvider} from '../src/Components/UserContext/AuthContext';
 
 function App() {
   return (
