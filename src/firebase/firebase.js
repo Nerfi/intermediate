ec715f16faaa663f9dea9fa7b9a85b0221a7a079
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-09-30 16:18:26 +0200

    added  .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,1 +1,24 @@
-//AÚN NO HE CONECTADO LA BASE DE DATOS AQUI
+import firebase from 'firebase';
+import 'firebase/firestore';
+//importing storage in order to upload img
+import 'firebase/storage';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+//initilalizing the app
+firebase.initializeApp(firebaseConfig);
+
+//initializing firebase storage
+const storage = firebase.storage();
+
+export {storage, firebase as default}
